1c4f8ab1f2b475314d1ca42c08280fb5b5fd1945
Shaun <mimisa00@gmail.com>
2025-09-12 10:10:33 +0800

    Update run_gcp-fw-bot.sh

--- 60 telegram knowledge/bot/run_gcp-fw-bot.sh
+++ 60 telegram knowledge/bot/run_gcp-fw-bot.sh
@@ -16,16 +16,12 @@ docker build -t ${ap_name}:latest /home/fusion-ap/${ap_name}/
 docker run -d \
   --name ${ap_name} \
   --restart=always --log-opt max-size=10m --log-opt max-file=10 \
-  -e TELEGRAM_TOKEN="6395609926:AAHnUPPor-KHY7yPqYQduijBd1Pil2OIt_Q" \
+  -e TELEGRAM_TOKEN="6395609***:***" \
   -e API_URL="https://fw-trigger-whitelist-172507983612.asia-east1.run.app" \
-  -e API_AUTH="Bearer k2jewrtdpIWASQ0gwEo7_mYfHVgpV69q" \
+  -e API_AUTH="Bearer k2jewrtdpIWASQ0gwEo7_mYfHVgp***" \
   -e ALLOWED_CHAT_IDS="1002529578334" \
-  -e ALLOWED_USERS="478605799","838268769","1272303435","5336268205","7208095646" \
+  -e ALLOWED_USERS="123","456" \
   -v /home/fusion-ap/${ap_name}/coms.txt:/app/coms.txt \
   -v /home/fusion-ap/${ap_name}/bot.py:/app/bot.py \
   -v /home/fusion-ap/${ap_name}/fw.log:/app/fw.log \
   ${ap_name}:latest
-
-#
-#ALLOWED_USERS="478605799","838268769","1272303435","5336268205","7208095646"
-#ALLOWED_USERS="shaun"    ,"nick"     ,"gino"      ,"hark"      ,"elisa"
